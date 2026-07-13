b20519ee91bcde06cda4750dbf73df8656c80c5f
Gorodnovdaniil <danyagorodnov@yandex.ru>
2026-07-13 16:28:53 +0300

    Код для визуализации графиков thinknotdraw

---
+++ statistics/visualisation/graphiscs.py
@@ -0,0 +1,20 @@
+# быстрое рисование графиков 
+# Создана библиотека для рисования графиков thinknotdraw
+
+#откройте консоль на компьютере или выполняйте в приложении: pip install pandas matplotlib numpy seaborn thinknotdraw
+#сделайте используемом python редакторе: from thinknotdraw import fast_draw
+#ниже представлен код для визуализации
+
+fast_draw(x, gtype='plot', kind='con', line=False, col=True, name='None')
+
+#x - ваши данные
+
+#gtype (plot/func) - рисовать плотность или функцию
+
+#kind (con/dis) - дискретные или непрерывные данные
+
+#line (True/False) - рисовать линию
+
+#col (True/False) - рисовать колонны
+
+#name - название графика
